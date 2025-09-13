7c11199ea8a37eb7a1f4822f7cee575204cf107c
SandeepMarumudi <166957294+SandeepMarumudi@users.noreply.github.com>
2025-09-13 16:47:57 +0530

    cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({ 
-    origin: "https://finance-frontend-pied.vercel.app/",
+    origin: "https://finance-frontend-pied.vercel.app/", 
     credentials: true,
   })
 );
